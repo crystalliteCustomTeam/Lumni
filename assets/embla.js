01e9da95cfcd8078c1539254269805d8129994bb
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-07-31 00:55:26 +0500

    new changes

--- assets/embla.js
+++ assets/embla.js
@@ -2,4 +2,3 @@ const emblaNode = document.querySelector('#embla')
 const options = { dragFree: true, loop: true }
 const plugins = [EmblaCarouselAutoplay({ delay: 4000, stopOnInteraction: false, stopOnFocusIn: false })]
 const emblaApi = EmblaCarousel(emblaNode, options, plugins)
-console.log(emblaApi.slideNodes()) // Access API
